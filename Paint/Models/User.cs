efa79c238158d961924fd6ef8c5e129c7f1bfb95
Lan <weilan0123@outlook.com>
2026-06-15 21:20:15 +1000

    update to LINQ

--- Paint/Models/User.cs
+++ Paint/Models/User.cs
@@ -17,97 +17,41 @@ public class User
 
     public List<Order> GetMostExpensiveOrders()
     {
-        decimal mostExpensiveOrderPrice = OrderHistory[0].TotalPrice;
+        decimal mostExpensiveOrderPrice = OrderHistory.Max(order => order.TotalPrice);
 
-        foreach (Order order in OrderHistory)
-        {
-            if (order.TotalPrice > mostExpensiveOrderPrice)
-            {
-                mostExpensiveOrderPrice = order.TotalPrice;
-            }
-        }
-
-        List<Order> mostExpensiveOrders = [];
-
-        foreach (Order order in OrderHistory)
-        {
-            if (order.TotalPrice == mostExpensiveOrderPrice)
-            {
-                mostExpensiveOrders.Add(order);
-            }
-        }
-
-        return mostExpensiveOrders;
+        return OrderHistory
+            .Where(order => order.TotalPrice == mostExpensiveOrderPrice)
+            .ToList();
     }
 
     public Order GetLatestOrder()
     {
-        Order latestOrder = OrderHistory[0];
-
-        foreach(Order order in OrderHistory)
-        {
-            if (order.CreatedAt > latestOrder.CreatedAt)
-            {
-                latestOrder = order;
-            }
-        }
-
-        return latestOrder;
+        return OrderHistory
+            .OrderByDescending(order => order.CreatedAt)
+            .First();
     }
 
     public List<Payment> GetLowestPayment()
     {
-        decimal lowestPaymentPrice = PaymentHistory[0].PaymentAmount;
-
-        foreach (Payment payment in PaymentHistory)
-        {
-            if (payment.PaymentAmount < lowestPaymentPrice)
-            {
-                lowestPaymentPrice = payment.PaymentAmount;
-            }
-        }
-
-        List<Payment> lowestPayments = [];
-
-        foreach (Payment payment in PaymentHistory)
-        {
-            if (payment.PaymentAmount == lowestPaymentPrice)
-            {
-                lowestPayments.Add(payment);
-            }
-        }
-
-        return lowestPayments;
+        decimal lowestPrice = PaymentHistory.Min(payment => payment.PaymentAmount);
+        
+        return PaymentHistory
+            .Where(payment => payment.PaymentAmount == lowestPrice)
+            .ToList();
     }
 
     public Payment LatestPayment()
     {
-        Payment latestPayment = PaymentHistory[0];
-
-        foreach(Payment payment in PaymentHistory)
-        {
-            if (payment.CreatedAt > latestPayment.CreatedAt)
-            {
-                latestPayment = payment;
-            }
-        }
-
-        return latestPayment;
+        return PaymentHistory
+            .OrderByDescending(payment => payment.CreatedAt)
+            .First();
     }
 
     public List<Payment> GetMoreThanTenPayment()
     {
 
-        List<Payment> MoreThanTenPayments = [];
-
-        foreach (Payment payment in PaymentHistory)
-        {
-            if (payment.PaymentAmount > 10 )
-            {
-                MoreThanTenPayments.Add(payment);
-            }
-        }
-
-        return MoreThanTenPayments;
+        return PaymentHistory
+            .Where(payment => payment.PaymentAmount > 10)
+            .ToList();
     }
 }
